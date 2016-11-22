aa7070ab0ae199d7a4de4043bb88cce59708c31b
j-west <westja89@gmail.com>
2016-11-22 08:52:02 -0600

    Add variable to hold user input, if statement to check if the input is greater than 25.

--- coins.js
+++ coins.js
@@ -32,3 +32,17 @@ calculateButton.addEventListener('click', function() {
   for(var value in coinPurse) {
     coinPurse[value] = 0;
   }
+
+  // Variable to hold the user's input from the text input in index.html.  Multiplyed by 100 to work with integers
+  var amountToCalc = (userInput.value * 100);
+  
+  // If amountToCalc is greater than or equal to 25
+  if (amountToCalc >= 25) {
+    // Set coinPurse.quarters to the result of amountToCalc / 25.  Rounded down which will give the amount of quarters       
+    coinPurse.quarters = Math.floor(amountToCalc / 25);
+    // Multiple the amount of coinPurse.quarters by 25 to get the amount to be 
+    // subtracted from amountToCalc.. (3 quarters * 25 = 75 cents)
+    amountToCalc -= (coinPurse.quarters * 25);
+
+    // If amountToCalc is greater than or equal to 10
+  }
